0ca6e0439d3c7427c2f28e9fb1bc5e57568343f6
Denis <denis.yaremenko@huma.com>
2024-04-18 15:19:38 +0300

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Denis Yaremenko on 18.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
